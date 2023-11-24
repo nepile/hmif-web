a12e79e2338f69ed0ba2c5272057cd585e8ef5f1
Neville Jeremy <nevillejro.laia@gmail.com>
2023-11-25 02:21:54 +0700

    feat: add animate on scroll and fix the z-index of header

--- hmif-frontend/src/views/home/Main.js
+++ hmif-frontend/src/views/home/Main.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
@@ -10,7 +9,7 @@ function Home() {
       </Helmet>
 
       <div className="flex bg-radial-gradient px-6 py-8 justify-between lg:flex-nowrap flex-wrap items-center gap-4 lg:px-14 lg:py-0 py-0">
-        <div className="w-100 lg:w-1/2">
+        <div data-aos="fade-up" className="w-100 lg:w-1/2">
           <p className="text-lg mb-1 first-color">Universitas Sanata Dharma</p>
           <h1 className="text-3xl font-bold lg:text-5xl mb-3">
             Dinamika Bersama Himpunan Mahasiswa Informatika
@@ -33,7 +32,7 @@ function Home() {
           </div>
         </div>
 
-        <div>
+        <div data-aos="fade-zoom-in">
           <img
             src="/images/asset1.png"
             style={{ height: "100%", width: "100%", objectFit: "cover" }}
