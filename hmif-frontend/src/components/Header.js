75620249e3c1fcb5278a306cca29cd2f1a964aee
Neville Jeremy <nevillejro.laia@gmail.com>
2023-11-25 00:36:22 +0700

    fix: add react dependencies and update header routes

--- hmif-frontend/src/components/Header.js
+++ hmif-frontend/src/components/Header.js
@@ -1,25 +1,59 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
 function Header() {
-    return(
-        <div className="flex justify-between shadow-sm items-center bg-white px-8 p-5">
-            <div>
-                <h1 className="h1 text-xl font-bold">HMIF</h1>
-            </div>
-            <div className="text-lg gap-x-10 hidden md:flex">
-                <a href="">Home</a>
-                <a href="">Events</a>
-                <a href="">News</a>
-                <a href="">Aspirations</a>
-                <a href="">About Us</a>
-            </div>
-            <div>
-                <button className="bg-gray-800 text-white px-6 py-1 rounded-md hover:bg-gray-700 focus:outline-none focus:shadow-outline">
-                    Login
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex justify-between shadow-sm items-center bg-white px-8 p-5">
+      <div>
+        <h1 className="text-xl font-bold">
+          <NavLink to="/" activeClassName="active-link">
+            HMIF
+          </NavLink>
+        </h1>
+      </div>
+
+      <div className="text-lg gap-x-10 hidden md:flex">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/events"
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          Events
+        </NavLink>
+        <NavLink
+          to="/news"
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          News
+        </NavLink>
+        <NavLink
+          to="/aspirations"
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          Aspirations
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          About Us
+        </NavLink>
+      </div>
+
+      <div>
+        <button className="bg-gray-800 text-white px-6 py-1 rounded-md hover:bg-gray-700 focus:outline-none focus:shadow-outline">
+          <NavLink to="/login" activeClassName="active-link">
+            Login
+          </NavLink>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
-
-
---
+++ hmif-frontend/src/views/home/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+function Home() {
+  return (
+    <div>
+      <Helmet>
+        <title>ASEK</title>
+      </Helmet>
+
+      <h3>OKE</h3>
+    </div>
+  );
+}
+
+export default Home;
