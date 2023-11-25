9fe9280a905982197c31d95f437704588eda68cf
Neville Jeremy <nevillejro.laia@gmail.com>
2023-11-26 00:34:25 +0700

    feat(footer): add component footer inside core app file

---
+++ hmif-frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <div className="grid grid-cols-12">
+        <p className="text-4xl">Test Footer</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
