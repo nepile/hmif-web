c60675675895c9acdf9d5f2e022248b1e947fc91
Neville Jeremy <nevillejro.laia@gmail.com>
2023-12-12 23:36:32 +0700

    feat: create information section about partnership and rechange web scrollbar

--- hmif-frontend/src/views/home/elements/HomeLandingPage.jsx
+++ hmif-frontend/src/views/home/elements/HomeLandingPage.jsx
@@ -1,9 +1,12 @@
 function HomeLandingPage() {
   return (
     <>
-      <div className="flex justify-center lg:py-16 py-44 bg-radial-gradient">
+      <div className="flex justify-center items-center flex-col lg:py-16 py-44 bg-radial-gradient">
         <div className="w-[90%] grid grid-cols-12 justify-between items-center">
-          <div data-aos="fade-up" className="lg:col-span-6 md:col-span-10 col-span-12">
+          <div
+            data-aos="fade-up"
+            className="lg:col-span-6 md:col-span-10 col-span-12"
+          >
             <p className="text-2xl first-color">Universitas Sanata Dharma</p>
             <h1 className="text-3xl font-bold lg:text-5xl my-4">
               Dinamika Bersama{" "}
@@ -31,12 +34,47 @@ function HomeLandingPage() {
               </a>
             </div>
           </div>
-          <div data-aos="fade-zoom-in" className="lg:col-span-6 md:col-span-6 sm:col-span-8 ex:col-span-12 lg:pt-0 pt-10">
+          <div
+            data-aos="fade-zoom-in"
+            className="lg:col-span-6 md:col-span-6 sm:col-span-8 ex:col-span-12 lg:pt-0 pt-10"
+          >
             <img
               className="w-full"
               src="/images/asset1.png"
               alt="Himpunan Mahasiswa Informatika"
             ></img>
+          </div>
+        </div>
+        <div className="w-[90%] grid flex lg:items-end grid-cols-1 lg:grid-cols-2 gap-10 bg-gray-100 p-10 rounded-sm">
+          <div className="mb-5">
+            <p className="text-4xl font-bold">Bersinergi Bersama</p>
+            <p className="text-2xl first-color">
+              Instansi dan Organisasi terkait.
+            </p>
+          </div>
+
+          <div className="w-[90%] grid grid-cols-3 gap-10 mt-5">
+            <div className="aspect-w-1 aspect-h-1">
+              <img
+                src="/images/usd_logo.png"
+                className="w-full h-full object-cover"
+                alt="USD Logo"
+              />
+            </div>
+            <div className="aspect-w-1 aspect-h-1">
+              <img
+                src="/images/bemfst_logo.png"
+                className="w-full h-full object-cover"
+                alt="BEMFST Logo"
+              />
+            </div>
+            <div className="aspect-w-1 aspect-h-1">
+              <img
+                src="/images/logo.png"
+                className="w-full h-full object-cover"
+                alt="Logo"
+              />
+            </div>
           </div>
         </div>
       </div>
