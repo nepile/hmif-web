0d490053fc69fb5d13dd2f21516d29879269cddd
joanpurba <joanpurba4@gmail.com>
2023-12-14 20:59:49 +0700

    feat(home): fixing the responsive on tablet size

--- hmif-frontend/src/views/home/elements/HomeLandingPage.jsx
+++ hmif-frontend/src/views/home/elements/HomeLandingPage.jsx
@@ -3,15 +3,18 @@ function HomeLandingPage() {
     <>
       <div className="flex justify-center lg:py-16 py-44 bg-radial-gradient">
         <div className="w-[90%] grid grid-cols-12 justify-between items-center">
-          <div data-aos="fade-up" className="lg:col-span-6 md:col-span-10 col-span-12">
+          <div
+            data-aos="fade-up"
+            className="lg:col-span-6 md:col-span-10 col-span-12"
+          >
             <p className="text-2xl first-color">Universitas Sanata Dharma</p>
-            <h1 className="text-3xl font-bold lg:text-5xl my-4">
+            <h1 className="text-5xl font-bold lg:text-5xl my-4">
               Dinamika Bersama{" "}
               <span className="customUnderline2">
                 Himpunan Mahasiswa Informatika
               </span>
             </h1>
-            <p className="md:w-[500px] text-lg">
+            <p className="lg:w-[500px] text-lg">
               Website{" "}
               <span className="customUnderline2">
                 Himpunan Mahasiswa Informatika
@@ -31,7 +34,10 @@ function HomeLandingPage() {
               </a>
             </div>
           </div>
-          <div data-aos="fade-zoom-in" className="lg:col-span-6 md:col-span-6 sm:col-span-8 ex:col-span-12 lg:pt-0 pt-10">
+          <div
+            data-aos="fade-zoom-in"
+            className="lg:col-span-6 md:col-span-7 sm:col-span-8 ex:col-span-12 lg:pt-0 pt-10"
+          >
             <img
               className="w-full"
               src="/images/asset1.png"
