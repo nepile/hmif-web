00c287ac881291c64caaddfb65f0ed8056615d58
Neville Jeremy <nevillejro.laia@gmail.com>
2023-12-21 09:55:57 +0700

    feat: update layout news component

--- hmif-frontend/src/views/news/elements/NewsLandingPage.jsx
+++ hmif-frontend/src/views/news/elements/NewsLandingPage.jsx
@@ -1,28 +1,44 @@
-const NewsLandingPage = () =>{
-
-    return(
-        <div class="container mx-auto p-8 flex mt-32 max-w-screen-xl">
-            <div class="berita-utama rounded-lg lg:w-1/2 mr-4 lg:h-3/5">
-                <img class="h-full w-full object-cover rounded-lg" src="images/itdaysdummy.jpg" alt="" />
-            </div>
-        
-            <div class="berita-pecahan grid grid-cols-2 gap-2 lg:w-1/2 lg:h-2/6">
-                <div class="pecahan-1 rounded-lg lg:h-3/5">
-                    <img class="h-full w-full object-cover rounded-lg" src="images/itdaysdummy.jpg" alt="" />
-                </div>
-                <div class="pecahan-2 rounded-lg lg:h-3/5">
-                    <img class="h-full w-full object-cover rounded-lg" src="images/itdaysdummy.jpg" alt="" />
-                </div>
-                <div class="pecahan-1 rounded-lg lg:h-3/5">
-                    <img class="h-full w-full object-cover rounded-lg" src="images/itdaysdummy.jpg" alt="" />
-                </div>
-                <div class="pecahan-2 rounded-lg lg:h-3/5">
-                    <img class="h-full w-full object-cover rounded-lg" src="images/itdaysdummy.jpg" alt="" />
-                </div>
-            </div>
+const NewsLandingPage = () => {
+  return (
+    <div class="container grid lg:grid-cols-12 flex gap-7 mx-auto p-8 flex items-center mt-32 max-w-screen-xl">
+      <div className="col-span-6 bg-gray-100">
+        <div className="w-full h-80 rounded-lg bg-gray-600">
+          <img
+            src="images/itdaysdummy.jpg"
+            className="w-full h-full object-cover rounded-lg"
+          />
         </div>
-    
-    
-    )
-}
+      </div>
+
+      <div className="col-span-6">
+        <div className="grid grid-cols-12 grid-rows-2 gap-7 w-full lg:h-80">
+          <div className="col-span-12 lg:col-span-6 bg-gray-100 rounded-lg row-span-1">
+            <img
+              src="images/itdaysdummy.jpg"
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </div>
+          <div className="col-span-12 lg:col-span-6 bg-gray-100 rounded-lg row-span-1">
+            <img
+              src="images/itdaysdummy.jpg"
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </div>
+          <div className="col-span-12 lg:col-span-6 bg-gray-100 rounded-lg row-span-1">
+            <img
+              src="images/itdaysdummy.jpg"
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </div>
+          <div className="col-span-12 lg:col-span-6 bg-gray-100 rounded-lg row-span-1">
+            <img
+              src="images/itdaysdummy.jpg"
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default NewsLandingPage;
